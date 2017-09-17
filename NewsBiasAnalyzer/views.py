602f92688b695c25ddde91a3bd07025bf34d204e
iqover9000 <iqover9000@gmail.com>
2017-09-17 02:23:22 -0400

    Adding POST request to Django

--- NewsBiasAnalyzer/views.py
+++ NewsBiasAnalyzer/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 def index(request):
     indexFile = open("index.html")
     return HttpResponse(indexFile)
+
+def analyze(request):
+    return HttpResponse("hey")
