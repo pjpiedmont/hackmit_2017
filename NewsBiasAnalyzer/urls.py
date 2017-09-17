e154ef579266e19c51921734499d212432e92c8f
Zack Holbrook <zackh1998@gmail.com>
2017-09-17 01:26:31 -0400

    wrapper

--- NewsBiasAnalyzer/urls.py
+++ NewsBiasAnalyzer/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 from django.conf import settings
 from django.conf.urls.static import static
-
+import requests
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,3 @@
 source activate 3.6
+./bin/runDockerSentiment.sh
 python manage.py runserver 0.0.0.0:8000
