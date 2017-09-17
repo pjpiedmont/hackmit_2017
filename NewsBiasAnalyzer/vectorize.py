448ecf4fe3cd8d378aadc03ce94b5dc53936fea3
kylerlittle <kyler.little@wsu.edu>
2017-09-16 23:15:36 -0700

    vectorize update

--- NewsBiasAnalyzer/vectorize.py
+++ NewsBiasAnalyzer/vectorize.py
@@ -5,6 +5,8 @@ Created on Sat Sep 16 16:14:19 2017
 @author: kyler
 """
 
+from paddleWrapper import getSentimentDictionary, getSentiment
+import re
 
 # Create instance of object.
 # Call vectorizeArticle. Pass in the entire article as a string.
@@ -16,23 +18,33 @@ class vectorize:
         # Order of FeatureSet: QuotationFrequency, SentimentFrequency... 
         self.__featureset = []
         
-    def vectorizeArticle(self, articleStr, sentimentFct):
+    def vectorizeArticle(self, articleStr):
         # Perform Tests
         self.calcQuoteF(articleStr)
-        self.calcSentimentF(articleStr, sentimentFct)
+        self.calcSentimentF(articleStr)
         return self.__featureset;
     
     def calcQuoteF(self, articleStr):
         self.__featureset.append(float(articleStr.count("\"")) / len(articleStr))
      
-    def calcSentimentF(self, articleStr, fct):    
+    def calcSentimentF(self, articleStr):    
         numSentiments = 0
         sentenceList = articleStr.split('\n')
+        Dict = getSentimentDictionary('word_dict.txt')
         for sentence in sentenceList:
             if (len(sentence) > 1):         # in case of ellipses (...)
-                sentence.append('.')
-                sentimentVals = fct(sentence)       # Returns 2-tuple of probabilities of positive, negative respectively
+                wordList = re.sub("[^\w]", " ", sentence).split()
+                keyList = []                
+                for word in wordList:
+                    if word in Dict:
+                        keyList.append(Dict[word])    
+                sentimentVals = getSentiment(keyList)       # Returns 2-tuple of probabilities of positive, negative respectively                
                 thresholdDifference = 0.2
                 if (abs(sentimentVals[0] - sentimentVals[1]) > thresholdDifference):
                     numSentiments += 1
         return float(numSentiments) / len(sentenceList)
+        
+test = vectorize()
+vector = test.vectorizeArticle('My name is kyler. I like cookies.')
+print('Expected result: 0.5')
+print('Actual result:' + vector[1])
